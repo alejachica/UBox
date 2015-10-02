232ac0781c0ef3697c24d2f72fcc5af1b3217649
Javier Virviescas <virtoja13@hotmail.com>
2015-10-02 18:28:43 -0500

    ajuste campos booleanos

--- Operador/UBox-Web/src/co/edu/uniandes/umbrella/managedbeans/DocumentosPersonalesBean.java
+++ Operador/UBox-Web/src/co/edu/uniandes/umbrella/managedbeans/DocumentosPersonalesBean.java
@@ -42,7 +42,7 @@ public class DocumentosPersonalesBean {
 	        	DocumentoDTO documentoDTO = new DocumentoDTO();
 	        	documentoDTO.setDocumento(file.getContents());
 	        	documentoDTO.setFecha("");
-	        	documentoDTO.setFirmado((byte)0);
+	        	documentoDTO.setFirmado(false);
 	        	documentoDTO.setFkCarpeta(1);
 	        	documentoDTO.setFkUsuario(3);
 	        	documentoDTO.setIdTipoDocumento(new BigDecimal(1));
@@ -50,7 +50,7 @@ public class DocumentosPersonalesBean {
 	        	documentoDTO.setNombre(file.getFileName());
 	        	documentoDTO.setPalabrasClave("archivo1");
 	        	documentoDTO.setVersion("version1");
-	        	documentoDTO.setPapelera((byte)0);
+	        	documentoDTO.setPapelera(false);
 	        	documentoDTO.setSize((int)file.getSize());
 	        	documentosEJB.crearDocumento(documentoDTO);
 	            FacesMessage message = new FacesMessage("Succesful", file.getFileName() + " is uploaded.");
