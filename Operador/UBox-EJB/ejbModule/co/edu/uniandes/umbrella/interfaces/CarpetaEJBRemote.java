b01b0b5cfd78b3fd9688bad2379d099a557c8fe3
Javier Virviescas <virtoja13@hotmail.com>
2015-10-02 19:05:24 -0500

    Se crearon nuevos metodos a la interface

--- Operador/UBox-EJB/ejbModule/co/edu/uniandes/umbrella/interfaces/CarpetaEJBRemote.java
+++ Operador/UBox-EJB/ejbModule/co/edu/uniandes/umbrella/interfaces/CarpetaEJBRemote.java
@@ -1,5 +1,7 @@
 package co.edu.uniandes.umbrella.interfaces;
 
+import java.util.List;
+
 import javax.ejb.Remote;
 import co.edu.uniandes.umbrella.dto.CarpetaDTO;
 import co.edu.uniandes.umbrella.entidades.Carpeta;
@@ -14,4 +16,7 @@ public interface CarpetaEJBRemote {
 	public void eliminarCarpeta(int idCarpeta) throws Exception;
 	
 	public void crearCarpeta(CarpetaDTO carpetaDTO, int idUsuario)throws Exception;
+	
+	public List<CarpetaDTO> carpetasXUsuario(int idUsuario) throws Exception;
+	
 }
