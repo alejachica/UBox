1ca625cbf107f8b83faf19693c32b9e6ffe22707
Aleja.Chica <aleja.chika@gmail.com>
2015-10-17 16:05:33 -0500

    Se crea servicio con la informacion para compartir un documento

--- Centralizador/Centralizador-EJB/ejbModule/co/edu/uniandes/umbrella/ejb/TransaccionesEJB.java
+++ Centralizador/Centralizador-EJB/ejbModule/co/edu/uniandes/umbrella/ejb/TransaccionesEJB.java
@@ -1,6 +1,7 @@
 package co.edu.uniandes.umbrella.ejb;
 
 import javax.ejb.Stateless;
+import javax.jws.WebService;
 
 import co.edu.uniandes.umbrella.dto.ServiciosOperadorUsuarioDTO;
 import co.edu.uniandes.umbrella.entidades.Operador;
@@ -11,6 +12,7 @@ import co.edu.uniandes.umbrella.interfaces.TransaccionesEJBRemote;
  * Session Bean implementation class InstitucionesEJB
  */
 @Stateless
+@WebService
 public class TransaccionesEJB implements TransaccionesEJBRemote, TransaccionesEJBLocal {
 
 	public Operador consultarOperadorUsuarioParaCompartir(
