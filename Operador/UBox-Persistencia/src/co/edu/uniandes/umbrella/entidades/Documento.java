70d039c2da390dcd1f1928d08394e4d09ecaab96
Javier Virviescas <virtoja13@hotmail.com>
2015-10-18 19:53:50 -0500

    Se quito import sin utilizar

--- Operador/UBox-Persistencia/src/co/edu/uniandes/umbrella/entidades/Documento.java
+++ Operador/UBox-Persistencia/src/co/edu/uniandes/umbrella/entidades/Documento.java
@@ -2,7 +2,6 @@ package co.edu.uniandes.umbrella.entidades;
 
 import java.io.Serializable;
 import javax.persistence.*;
-import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
