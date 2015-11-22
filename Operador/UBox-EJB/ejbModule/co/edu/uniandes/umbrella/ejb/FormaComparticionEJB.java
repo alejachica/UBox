fce3d3174d380516f83a47fdb302f744d9df8361
Javier Virviescas <virtoja13@hotmail.com>
2015-11-21 21:18:15 -0500

    se quita import

--- Operador/UBox-EJB/ejbModule/co/edu/uniandes/umbrella/ejb/FormaComparticionEJB.java
+++ Operador/UBox-EJB/ejbModule/co/edu/uniandes/umbrella/ejb/FormaComparticionEJB.java
@@ -2,7 +2,6 @@ package co.edu.uniandes.umbrella.ejb;
 
 import java.util.List;
 
-import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
